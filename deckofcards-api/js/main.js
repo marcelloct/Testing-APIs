7bf8f92f4178b650021326c8424efd829fba68d3
Marcello C <marcello_ct@hotmail.com>
2025-09-10 18:41:38 -0300

    Adding War function, if players draw, get 2 more cards

--- deckofcards-api/js/main.js
+++ deckofcards-api/js/main.js
@@ -30,6 +30,8 @@ function drawTwo() {
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
+      document.querySelector("#player1War").classList.add("hidden");
+      document.querySelector("#player2War").classList.add("hidden");
 
       // Display the card
       document.querySelector("#player1").src = data.cards[0].image;
@@ -48,6 +50,9 @@ function drawTwo() {
         displayScore.innerText = `P1: ${player1Score} | P2: ${(player2Score += 1)}`;
       } else {
         displayRoundResult.innerText = "Draw";
+        dealCardButton.classList.add("hidden");
+        document.querySelector("#war").classList.remove("hidden");
+        document.querySelector("#war").addEventListener("click", war);
       }
 
       // Display Remaining Cards
@@ -88,11 +93,60 @@ function resetGame() {
   displayFinalScore.innerText = "War Game";
   displayRoundResult.innerText = "";
   displayScore.innerText = "";
-  displayRound.innerText = "";
   dealCardButton.classList.toggle("hidden");
+}
+
+function war() {
+  const url = `https://www.deckofcardsapi.com/api/deck/${deckID}/draw/?count=2`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      dealCardButton.classList.remove("hidden");
+      document.querySelector("#war").classList.add("hidden");
+
+      // Display the card
+      document.querySelector("#player1War").classList.remove("hidden");
+      document.querySelector("#player2War").classList.remove("hidden");
+      document.querySelector("#player1War").src = data.cards[0].image;
+      document.querySelector("#player2War").src = data.cards[1].image;
+
+      // Get the value of each card and pass throw a function to convert to a number
+      let player1Val = convertToNum(data.cards[0].value);
+      let player2Val = convertToNum(data.cards[1].value);
+
+      // Check who is the winner and increment the score
+      if (player1Val > player2Val) {
+        displayRoundResult.innerText = "Player 1 Wins";
+        displayScore.innerText = `P1: ${
+          player1Score > 0 ? (player1Score *= 2) : (player1Score += 1)
+        } | P2: ${player2Score}`;
+        document.querySelector("#war").classList.add("hidden");
+      } else if (player1Val < player2Val) {
+        displayRoundResult.innerText = "Player 2 Wins";
+        displayScore.innerText = `P1: ${player1Score} | P2: ${
+          player2Score > 0 ? (player2Score *= 2) : (player2Score += 1)
+        }`;
+        document.querySelector("#war").classList.add("hidden");
+      }
+
+      // Display Remaining Cards
+      displayRemainingCards.innerText = `Cards Remainig: ${data.remaining}`;
+
+      // Reload the page if no remaining cards to draw
+      if (data.remaining === 0) {
+        displayFinalScore.innerText =
+          "Not enough cards remaining to draw, page wil be reloaded";
+        setTimeout(function () {
+          location.reload();
+        }, 5000);
+      }
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
 }
 
 // local store the deck id to use the same deck always
 
-// if war, draw another card and the winner double the points
 // *** probably assign the two values from each player cards in an array and sum them, next multiply the score by 2
